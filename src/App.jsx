a947c41c625001c9bf83761db9f10f487f659e72
carlos <orozcoalexander1996@gmail.com>
2021-11-30 14:58:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App1.jsx
@@ -0,0 +1,13 @@
+
+import About from "./components/about/About";
+
+function App1() {
+  return (
+    <div className="app">
+      <About />
+
+    </div>
+  );
+}
+
+export default App1;
