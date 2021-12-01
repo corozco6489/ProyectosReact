e2adaf3b8c713453183c8ce22024cbe3ca02e0b8
carlos <orozcoalexander1996@gmail.com>
2021-11-30 20:25:09 -0500

    portafolio-react

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,108 @@
+import Topbar from "../topbar/Topbar";
+import "./contact.scss";
+import {
+  Facebook,
+  Instagram,
+  LinkedIn,
+  YouTube,
+  Email,
+  Phone,
+  LocationOn,
+} from "@material-ui/icons";
+function Contact() {
+  return (
+    <div className="contacto">
+      <Topbar />
+
+      <section className="contact">
+        <h1 className="heading">
+          {" "}
+          contact <span>me</span>{" "}
+        </h1>
+
+        <div className="row">
+          <div className="info-container">
+            <h1>get in touch</h1>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+              dolorem sunt sapiente vel minus eaque voluptate fugit corrupti
+              omnis tempora?
+            </p>
+
+            <div className="box-container">
+              <div className="box">
+                <i className="fas fa-map">
+                  <LocationOn className="fas fa-user" />{" "}
+                </i>
+                <div className="info">
+                  <h3>address :</h3>
+                  <p>mumbai, india - 400104</p>
+                </div>
+              </div>
+
+              <div className="box">
+                <i className="fas fa-envelope">
+                  <Email className="fas fa-user" />{" "}
+                </i>
+                <div className="info">
+                  <h3>email :</h3>
+                  <p>sanashaikh@gmail.com</p>
+                </div>
+              </div>
+
+              <div className="box">
+                <i className="fas fa-phone">
+                  <Phone className="fas fa-user" />{" "}
+                </i>
+                <div className="info">
+                  <h3>number :</h3>
+                  <p>+123-456-7890</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="share">
+              <a href="#" >
+                <YouTube className="fas fa-user" />{" "}
+              </a>
+              <a href="#" >
+                <LinkedIn className="fas fa-user" />{" "}
+              </a>
+              <a href="#" >
+                <Facebook className="fas fa-user" />{" "}
+              </a>
+              <a href="#">
+                <Instagram className="fas fa-user" />{" "}
+              </a>
+            </div>
+          </div>
+
+          <form action="">
+            <div className="inputBox">
+              <input type="text" placeholder="your name" />
+              <input type="number" placeholder="your number" />
+            </div>
+
+            <div className="inputBox">
+              <input type="email" placeholder="your email" />
+              <input type="text" placeholder="your subject" />
+            </div>
+
+            <textarea
+              name=""
+              placeholder="your message"
+              id=""
+              cols="30"
+              rows="10"
+            ></textarea>
+
+            <input type="submit" value="send message" className="btn" />
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Contact;
